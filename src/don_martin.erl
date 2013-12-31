8ecce046e9385942d8a2823282ba5cd5147175ba
John Haugeland <stonecypher@gmail.com>
2013-12-30 23:26:46 -0800

    First Don Draft

---
+++ src/don_martin.erl
@@ -0,0 +1,174 @@
+
+-module(don_martin).
+
+
+
+
+
+-export([
+
+    word/1
+
+]).
+
+
+
+
+
+word(1) -> "aarh";
+word(2) -> "ahh";
+word(3) -> "arargh";
+word(4) -> "bash";
+word(5) -> "beedoop";
+word(6) -> "blamp";
+word(7) -> "blib";
+word(8) -> "blink";
+word(9) -> "blonk";
+word(10) -> "blort";
+word(11) -> "blut";
+word(12) -> "boong";
+word(13) -> "borfft";
+word(14) -> "brrrapp";
+word(15) -> "caw";
+word(16) -> "chukkunk";
+word(17) -> "da";
+word(18) -> "dap";
+word(19) -> "deebe";
+word(20) -> "dig";
+word(21) -> "dikka";
+word(22) -> "dimpah";
+word(23) -> "dit";
+word(24) -> "dokka";
+word(25) -> "doodle";
+word(26) -> "doom";
+word(27) -> "doomp";
+word(28) -> "doonk";
+word(29) -> "doop";
+word(30) -> "eck";
+word(31) -> "fagwoosh";
+word(32) -> "fak";
+word(33) -> "faroolana";
+word(34) -> "fitzrower";
+word(35) -> "fling";
+word(36) -> "foing";
+word(37) -> "fomp";
+word(38) -> "fowm";
+word(39) -> "frack";
+word(40) -> "fump";
+word(41) -> "fut";
+word(42) -> "fwask";
+word(43) -> "fween";
+word(44) -> "fweep";
+word(45) -> "fwiskitty";
+word(46) -> "fwizz";
+word(47) -> "fwoof";
+word(48) -> "ga";
+word(49) -> "gahoff";
+word(50) -> "gak";
+word(51) -> "galink";
+word(52) -> "gashklitz";
+word(53) -> "gashplutzga";
+word(54) -> "gasploosh";
+word(55) -> "ging";
+word(56) -> "gishklork";
+word(57) -> "glangadang";
+word(58) -> "gleep";
+word(59) -> "glikity";
+word(60) -> "glink";
+word(61) -> "glip";
+word(62) -> "gloople";
+word(63) -> "glukkle";
+word(64) -> "gonk";
+word(65) -> "goyng";
+word(66) -> "gring";
+word(67) -> "hee";
+word(68) -> "ho";
+word(69) -> "huff";
+word(70) -> "hushle";
+word(71) -> "inkle";
+word(72) -> "kachoo";
+word(73) -> "kachunka";
+word(74) -> "katoonk";
+word(75) -> "kazash";
+word(76) -> "kik";
+word(77) -> "kikatik";
+word(78) -> "kipf";
+word(79) -> "klikrunk";
+word(80) -> "kreek";
+word(81) -> "krunch";
+word(82) -> "mabbit";
+word(83) -> "oggock";
+word(84) -> "ooga";
+word(85) -> "oont";
+word(86) -> "padap";
+word(87) -> "phelop";
+word(88) -> "ping";
+word(89) -> "pitooie";
+word(90) -> "plapf";
+word(91) -> "ploom";
+word(92) -> "plortch";
+word(93) -> "poffisss";
+word(94) -> "pok";
+word(95) -> "puffa";
+word(96) -> "pwof";
+word(97) -> "shash";
+word(98) -> "shik";
+word(99) -> "shkalink";
+word(100) -> "shkaloink";
+word(101) -> "shklik";
+word(102) -> "shkwitz";
+word(103) -> "shloop";
+word(104) -> "shompah";
+word(105) -> "shooka";
+word(106) -> "shpikkle";
+word(107) -> "shtork";
+word(108) -> "shwik";
+word(109) -> "skapasch";
+word(110) -> "sklitch";
+word(111) -> "sklizzorch";
+word(112) -> "sklortch";
+word(113) -> "skloshitty";
+word(114) -> "sklush";
+word(115) -> "sknikle";
+word(116) -> "skrink";
+word(117) -> "skroik";
+word(118) -> "skronch";
+word(119) -> "slapth";
+word(120) -> "spashle";
+word(121) -> "spazoosh";
+word(122) -> "spla";
+word(123) -> "splapple";
+word(124) -> "splatch";
+word(125) -> "splazitch";
+word(126) -> "splesh";
+word(127) -> "splosh";
+word(128) -> "splurp";
+word(129) -> "sprat";
+word(130) -> "sprazot";
+word(131) -> "sproingdoink";
+word(132) -> "stoof";
+word(133) -> "swipadda";
+word(134) -> "swish";
+word(135) -> "tffp";
+word(136) -> "thhhut";
+word(137) -> "thlip";
+word(138) -> "thomp";
+word(139) -> "thoom";
+word(140) -> "thork";
+word(141) -> "tika";
+word(142) -> "ting";
+word(143) -> "tink";
+word(144) -> "tood";
+word(145) -> "trump";
+word(146) -> "twop";
+word(147) -> "varoom";
+word(148) -> "wee";
+word(149) -> "yeech";
+word(150) -> "yip";
+word(151) -> "zat";
+word(152) -> "zgluk";
+word(153) -> "zich";
+word(154) -> "zikka";
+word(155) -> "zlitz";
+word(156) -> "zock";
+word(157) -> "zwot".
